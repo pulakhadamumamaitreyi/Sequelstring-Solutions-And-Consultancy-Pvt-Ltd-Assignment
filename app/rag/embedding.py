0f6802a74cd01157a77752391310ed72c1d4a9d9
pulakhadamumamaitreyi <160453397+pulakhadamumamaitreyi@users.noreply.github.com>
2026-07-27 12:02:04 +0530

    Implement EmbeddingModel using SentenceTransformer

---
+++ app/rag/embedding.py
@@ -0,0 +1,21 @@
+from sentence_transformers import SentenceTransformer
+
+
+class EmbeddingModel:
+
+    def __init__(self):
+        self.model = SentenceTransformer(
+            "sentence-transformers/all-MiniLM-L6-v2"
+        )
+
+    def encode(self, texts):
+
+        if isinstance(texts, str):
+            texts = [texts]
+
+        embeddings = self.model.encode(
+            texts,
+            convert_to_numpy=True
+        )
+
+        return embeddings.tolist()
